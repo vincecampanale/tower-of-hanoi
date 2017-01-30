f0e35560c714816db4a767b075c127696aa8e0ff
Vince Campanale <vcampanale0294@gmail.com>
2017-01-30 11:49:27 -0500

    added construct disc function and built the tower

--- hanoi.js
+++ hanoi.js
@@ -1,4 +1,4 @@
-var camera, scene, renderer, cube, floor, ambientLight;
+var camera, scene, renderer, floor, control, discArray = [];
 var keyboard = {};
 var USE_WIREFRAME = false;
 var player = {
@@ -24,19 +24,54 @@ function init() {
   addPlatformAt("center"); //add center platform to the scene
   letThereBeLight(); //add a point light to the scene
 
+  addDisc(4, "blue", 1); //size 4 cylinder, color blue, bottom of stack
+  addDisc(3, "green", 2); //size 3 cylinder, color green, 2nd stack position
+  addDisc(2, "yellow", 3); //size 2 cylinder, color yellow, 3rd stack position
+  addDisc(1, "white", 4); //size 1 cylinder, color white, top stack position
+
   buildRenderer(); //build the renderer
   animate(); //set everything in motion
+
+  enableControls();
+  console.log(discArray);
 }
 
-function addCylinder() { //add a new cylinder on to the stack with size (1, 2, 3, or 4) 1 smallest - 4 biggest
+function enableControls() {
+  //TODO: Fix orbit controls
+  // Orbit controls make things a little unusual/complicated because they mess with camera and allow rotation to below the surface of the place
+  // controls = new THREE.OrbitControls( camera );
+  // controls.enableDamping = true;
+  // controls.dampingFactor = 0.25;
+  // controls.enableZoom = false;
 
+  //Transform controls allow the user to move the disks
+  control = new THREE.TransformControls( camera, renderer.domElement );
+  control.setMode("translate");
+
+}
+
+function addDisc(number, color, stackPosition) { //add a new cylinder on to the stack with number (1 smallest - 4 biggest), a color, and the position in the stack (1 - bottom, 4 - top)
+  //instantiate a cylinder (radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded, thetaStart, thetaLength)
+  var discGeometry = new THREE.CylinderGeometry(number, number, 0.8, 50, false);
+  var discMaterial = new THREE.MeshPhongMaterial({color: color, wireframe: false});
+  var disc = new THREE.Mesh( discGeometry, discMaterial );
+  var y = stackPosition === 1 ? 0.3 + (stackPosition * disc.geometry.parameters.height / 2) :
+                                (stackPosition * (disc.geometry.parameters.height)); //place the cylinder on top of the platform
+  var x = 10; //place the cylinder in the center of the left platform
+  var z = 0; //place it in the center of the floor
+
+  disc.position.set(x, y, z); //set the position of the cylinder
+  disc.receiveShadow = true;
+  disc.castShadow = true;
+  scene.add(disc);
+  discArray.push(disc);
 }
 
 function addPlatformAt(position) { //add a new platform at "left", "right", or "center"
   //instantiate a cylinder (radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded, thetaStart, thetaLength)
   var platformGeometry = new THREE.CylinderGeometry(4, 4, 0.3, 50, false);
-  var platformMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: USE_WIREFRAME});
-  var platform = new THREE.Mesh(platformGeometry, platformMaterial);
+  var platformMaterial = new THREE.MeshPhongMaterial({color: 0x001f3f, wireframe: USE_WIREFRAME});
+  var platform = new THREE.Mesh( platformGeometry, platformMaterial );
   var y = platform.geometry.parameters.height / 2; //bottom of platform touches floor
   var x = position === "left" ? -10:
           position === "right" ? 10:
@@ -44,33 +79,36 @@ function addPlatformAt(position) { //add a new platform at "left", "right", or "
           0; //if nothing is provided, place platform in center
   var z = 0; //place it in the center of the floor
 
-  platform.position.set(x, y, 0); //set the position of the platform
+  platform.position.set(x, y, z); //set the position of the platform
   platform.receiveShadow = true; //allow the platform to receive shadows
   platform.castShadow = true; //allow the platform to cast shadows
-  scene.add(platform);
+  scene.add(platform); //add the platform to the scene
 }
 
 function addFloor() {
   //instantiate a plane (width, height, widthSegments [opt], heightSegments [opt])
-  var planeGeometry = new THREE.PlaneGeometry(1000, 500, 20, 20);
-  var planeMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: USE_WIREFRAME});
+  var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 20, 20);
+  var planeMaterial = new THREE.MeshPhongMaterial({color: 0x001f3f, wireframe: USE_WIREFRAME});
   floor = new THREE.Mesh(planeGeometry, planeMaterial);
-  floor.rotation.x -= Math.PI / 2;
+  floor.rotation.x -= Math.PI / 2; //turn the floor horizontally (make sure it's facing up)
   floor.receiveShadow = true; //tell the floor to receive shadows (note: floor does not need to cast shadows)
-  scene.add(floor);
+  floor.scale.set(1000,1000,1000); //have the floor scale to appear to take up more space
+  scene.add(floor); //add the floor to the scene
 }
 
 function letThereBeLight() {
   //instantiate a new point light (color [opt], intensity [opt], distance, decay)
-  pointLight = new THREE.PointLight(0xffffff, 1.2, 100, 2); //decay = 2 for realistic light falloff
+  var pointLight = new THREE.PointLight(0xffffff, 1.2, 100, 2); //decay = 2 for realistic light falloff
   pointLight.position.set(-3, 6, -6); //set the position of the light
   pointLight.castShadow = true; //allow the light to cast a shadow
   pointLight.shadow.camera.near = 0.1; //cast a small shadow when near
   pointLight.shadow.camera.far = 25; //cast a large shadow when far
-  scene.add(pointLight); //add the light to the scene
+  scene.add(pointLight); //add the point light to the scene
 
   //instantiate a new ambient light
-  ambientLight = new THREE.AmbientLight();
+  var ambientLight = new THREE.AmbientLight( 0xffffff );
+  scene.add( ambientLight );
+
 }
 
 function buildRenderer() {
@@ -135,6 +173,21 @@ function keyDown(event){
 function keyUp(event){
   keyboard[event.keyCode] = false;
 }
+
+//credit for this function http://barkofthebyte.azurewebsites.net/post/2014/05/05/three-js-projecting-mouse-clicks-to-a-3d-scene-how-to-do-it-and-how-it-works
+// function onDocumentMouseDown( event ) {
+//   var x = event.clientX / (screenWidth * 2) - 1;
+//   var y = - ( event.clientY / (screenHeight * 2) + 1 );
+//   var z = 0.5;
+//   var mouse2D = new THREE.Vector3( x, y );
+//
+//   var raycaster = new THREE.Raycaster();
+//   raycaster.setFromCamera( mouse2D , camera );
+//   var intersects = raycaster.intersectObjects(  );
+//   if(intersects > 0){
+//     console.log(intersects);
+//   }
+// }
 
 window.addEventListener('keydown', keyDown);
 window.addEventListener('keyup', keyUp);
